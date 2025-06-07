f5382a5a64a0f5d9059fd2b31ca1ecf71e1e0ebe
abeljim <abelj1@uci.edu>
2025-06-07 00:56:29 -0700

    started rust port

--- devenv.nix
+++ devenv.nix
@@ -1,39 +1,27 @@
 {
   pkgs,
   lib,
-  config,
-  inputs,
+  # config,
+  # inputs,
   ...
 }: {
   packages = with pkgs; [
-    # GTK development
-    gtk4
-    pkg-config
-    gcc
-
-    # Wayland support
     wayland
-    wayland-protocols
     libxkbcommon
 
-    # Additional libraries that GTK4 commonly needs
-    glib
-    cairo
-    pango
-    gdk-pixbuf
-    json-glib # For JSON parsing
+    vulkan-loader
 
     # Development tools
-    gdb
+    cargo-insta
   ];
 
-  env = {
-    PKG_CONFIG_PATH = "${pkgs.gtk4.dev}/lib/pkgconfig:${pkgs.wayland.dev}/lib/pkgconfig";
-    # Ensure Wayland backend is preferred
-    GDK_BACKEND = "wayland";
-  };
+  env.LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${lib.makeLibraryPath [pkgs.wayland pkgs.vulkan-loader pkgs.libxkbcommon]}";
 
-  languages.c = {
+  languages.rust = {
     enable = true;
+    # https://devenv.sh/reference/options/#languagesrustchannel
+    channel = "nightly";
+
+    components = ["rustc" "cargo" "clippy" "rustfmt" "rust-analyzer"];
   };
 }
